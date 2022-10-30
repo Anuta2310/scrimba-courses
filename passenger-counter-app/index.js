9890951dfe6872c8e18dbab280a8d2b5dd570edc
Anna <116761052+Anuta2310@users.noreply.github.com>
2022-10-31 01:43:42 +0300

    changed innerText to textContent 	modified:   passenger-counter-app/index.html 	modified:   passenger-counter-app/index.js

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,12 +1,14 @@
 let countEl = document.getElementById("count-el")
-
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
   count += 1
-  countEl.innerText = count
+  countEl.textContent = count
 }
 
 function save() {
   console.log(count)
+  let saveOutput = " " + count + " -"
+  saveEl.textContent += saveOutput
 }
