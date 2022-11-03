0cbbb2accb8da2b3bd5902a423945aa8ebc957cb
Anna <116761052+Anuta2310@users.noreply.github.com>
2022-11-03 13:55:26 +0300

    added password length input feature

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,15 +1,22 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let pswdOneEl = document.getElementById("pswd-one")
-let pswdTwoEl = document.getElementById("pswd-two")
-document.getElementById("generate-btn").addEventListener("click", function()
+const pswdOneEl = document.getElementById("pswd-one")
+const pswdTwoEl = document.getElementById("pswd-two")
+const generateBtn = document.getElementById("generate-btn")
+const lengthInput = document.getElementById("length-input")
+
+let pswdLength = lengthInput.valueAsNumber
+lengthInput.addEventListener("input", function(){
+  pswdLength = lengthInput.valueAsNumber
+  return pswdLength
+})
+
+generateBtn.addEventListener("click", function()
 { let pswdOne = getRandomPswd()
   let pswdTwo = getRandomPswd()
   pswdOneEl.textContent = pswdOne
   pswdTwoEl.textContent = pswdTwo
 })
-
-let pswdLength = 15
 
 function getRandomChar() {
     let randomChar = Math.floor (Math.random() * characters.length) 
