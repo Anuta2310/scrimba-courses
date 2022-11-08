c07700fddabf2ca59b0bc497513efce89dc01b7e
Anna <116761052+Anuta2310@users.noreply.github.com>
2022-11-08 22:21:53 +0300

    rendered out posts with JS, dblclick likes

--- oldagram/index.js
+++ oldagram/index.js
@@ -28,3 +28,53 @@ const posts = [
     }
 ]
 
+document.getElementById("user-full-name").innerHTML = posts[0].name;
+document.getElementById("username").innerHTML = posts[0].username;
+document.getElementById("location").innerHTML = posts[0].location;
+document.getElementById("avatar").src = posts[0].avatar;
+document.getElementById("post").src = posts[0].post;
+document.getElementById("comment").innerHTML = posts[0].comment;
+document.getElementById("likes").innerHTML = `${posts[0].likes} likes` 
+
+document.getElementById("user-full-name1").innerHTML = posts[1].name;
+document.getElementById("username1").innerHTML = posts[1].username;
+document.getElementById("location1").innerHTML = posts[1].location;
+document.getElementById("avatar1").src = posts[1].avatar;
+document.getElementById("post1").src = posts[1].post;
+document.getElementById("comment1").innerHTML = posts[1].comment;
+document.getElementById("likes1").innerHTML = `${posts[1].likes} likes` 
+
+document.getElementById("user-full-name2").innerHTML = posts[2].name;
+document.getElementById("username2").innerHTML = posts[2].username;
+document.getElementById("location2").innerHTML = posts[2].location;
+document.getElementById("avatar2").src = posts[2].avatar;
+document.getElementById("post2").src = posts[2].post;
+document.getElementById("comment2").innerHTML = posts[2].comment;
+document.getElementById("likes2").innerHTML = `${posts[2].likes} likes` 
+
+let likes = posts[0].likes
+function increment(){
+    likes += 1
+    document.getElementById("likes").innerHTML = `${likes} likes`
+}
+document.getElementById("post").addEventListener("dblclick", function(){
+    increment()
+})
+
+let likes1 = posts[1].likes
+function increment1(){
+    likes1 += 1
+    document.getElementById("likes1").innerHTML = `${likes1} likes`
+}
+document.getElementById("post1").addEventListener("dblclick", function(){
+    increment1()
+})
+
+let likes2 = posts[2].likes
+function increment2(){
+    likes2 += 1
+    document.getElementById("likes2").innerHTML = `${likes2} likes`
+}
+document.getElementById("post2").addEventListener("dblclick", function(){
+    increment2()
+})
