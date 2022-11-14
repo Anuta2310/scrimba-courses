125fffa2879b20989150f4af50e60fdf8935abcb
Anna <116761052+Anuta2310@users.noreply.github.com>
2022-11-14 22:46:50 +0300

    rendering the cat meme + better readability of js

--- meme-picker/index.js
+++ meme-picker/index.js
@@ -3,9 +3,13 @@ import {catsData} from "/data.js"
 const emotionRadios = document.getElementById("emotion-radios")
 const getImageBtn = document.getElementById("get-image-btn")
 const gifsOnlyOption = document.getElementById("gifs-only-option")
+const memeModal = document.getElementById("meme-modal")
+const memeModalInner = document.getElementById("meme-modal-inner")
+const memeModalCloseBtn = document.getElementById("meme-modal-close-btn")
 
 emotionRadios.addEventListener("change", highlightCheckedOption)
-getImageBtn.addEventListener("click", getMatchingCatsArray)
+memeModalCloseBtn.addEventListener("click", closeModal) 
+getImageBtn.addEventListener("click", renderCat)
 
 function highlightCheckedOption(e){
   const radioArray = document.getElementsByClassName("radio")
@@ -15,11 +19,30 @@ function highlightCheckedOption(e){
   document.getElementById(e.target.id).parentElement.classList.add("highlight")
 }
 
+function closeModal(){
+  memeModal.style.display = "none"
+}
+
+function renderCat(){
+  const catObject = getSingleCatObject()
+  memeModalInner.innerHTML = 
+    `<img class="cat-img" src="./images/${catObject.image}" alt="${catObject.alt}">`
+  memeModal.style.display = "flex"
+}
+
+function getSingleCatObject(){
+  const catsArray = getMatchingCatsArray()
+  if (catsArray.length === 1){
+    return catsArray[0]
+  } else {
+    return catsArray[Math.floor(Math.random()*catsArray.length)]
+  }
+}
+
 function getMatchingCatsArray(){
   if (document.querySelector("input[type='radio']:checked")){
   const checkedRadio = document.querySelector("input[type='radio']:checked").value
   const isGif = gifsOnlyOption.checked
-
   const matchingCatsArray = catsData.filter(function(cat){
     if (isGif){return cat.emotionTags.includes(checkedRadio) && cat.isGif
     } else {return cat.emotionTags.includes(checkedRadio)}
