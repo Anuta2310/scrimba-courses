bb57db8cf3c082e9996cf1f02d7414be7b6f013e
Anna <116761052+Anuta2310@users.noreply.github.com>
2022-12-09 18:32:35 +0300

    finished mobile version

--- drone-page/index.js
+++ drone-page/index.js
@@ -1,1 +1,10 @@
-// javascript
+const menuBtn = document.querySelector('.menu-btn')
+      closeBtn = document.querySelector('.close-btn')
+      navPanel = document.querySelector('.nav-panel');
+
+menuBtn.addEventListener('click', () => {
+  navPanel.style.visibility = 'visible'
+})
+closeBtn.addEventListener('click', () => {
+  navPanel.style.visibility = 'hidden'
+})
